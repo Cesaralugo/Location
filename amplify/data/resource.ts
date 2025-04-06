11937d8e4fd0c416700e0988ce9b92235eca93c5
Cesar <cesaralugo31@gmail.com>
2025-04-06 14:37:21 -0500

    Change in resource.ts defaultauthorization mode

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -20,7 +20,10 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'userPool'
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: {
+      expiresInDays:30
+    }
   }
 });
 
